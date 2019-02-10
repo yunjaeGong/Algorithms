3fef89e66047145111c63397779b60e0faa98e24
yunjaeGong <llegokid@naver.com>
2019-02-10 21:47:41 +0900

    주석 추가

--- 9011_Order/main.cpp
+++ 9011_Order/main.cpp
@@ -12,15 +12,16 @@ int main() {
         for(int i=0;i<n;++i) {
             A[i] = i+1; cin >> r[i];
         }
-        for(int i=n-1;i>=0;--i) { // 뒤에서부터
-            if(r[i]>i || r[i]>=A.size()) {
+        for(int i=n-1;i>=0;--i) { // 마지막 원소부터 탐색
+            if(r[i]>i || r[i]>=A.size()) { // 수열을 만들 수 없으면
+                // ( 내(s[i])앞에 너무 많은 수가 필요할 때 -> r[i]>i 혹은 s[i] 후보 (A[]) 의 최댓값보다 더 많은 수가 (s[i]>n인 경우) 필요할 때
                 cout << "IMPOSSIBLE" << endl;
                 P = false; break;
             }
-            s[i] = A[r[i]]; //
-            A.erase(A.begin()+r[i]);
+            s[i] = A[r[i]]; // r[i]는 A(s[i]의 후보) 중 r[i] 조건을 만족하는 s[i]의 위치
+            A.erase(A.begin()+r[i]); // 사용한 s[i]는 제거 (중복 x)
         }
-        if(P) {
+        if(P) { // 수열을 만들 수 있으면
             for(int i=0;i<n;++i)
                 cout << s[i] << " ";
             cout << endl;
