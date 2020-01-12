17a436303bc02bb1cf895709774fcc2886cf5c85
YunjaeGong <llegokid@naver.com>
2020-01-12 23:59:23 +0900

    .

---
+++ 15651_N&M_3/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+using namespace std;
+short arr[8];
+int N, M;
+void A(int dep, int m) {
+    arr[dep] = m;
+    if(dep == M) {
+        for(int i=1;i<=M;++i)
+            printf("%hd ", arr[i]);
+        printf("\n");
+        return;
+    }
+    for(int i=1;i<=N;++i)
+        A(dep+1, i);
+}
+
+int main() {
+    scanf("%d%d", &N, &M);
+    A(0, 1);
+}
