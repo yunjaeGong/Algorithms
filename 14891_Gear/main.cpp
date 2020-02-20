07fceff5013de8f628dabf8ea420f56ff352e7a7
YunjaeGong <llegokid@naver.com>
2020-02-20 23:58:23 +0900

    .

--- 14891_Gear/main.cpp
+++ 14891_Gear/main.cpp
@@ -1,52 +1,49 @@
 #include <cstdio>
-short ind[4] = {0,};
+int ind[4] = {0,}; // 12시방향의 인덱스
 char gear[4][9];
 void rotate() { // r: 1 시계방향, -1 반시계방향
     int K,cnt=0;
     scanf("%d",&K);
     int n,r, left_p, right_p;
     for(int i=0;i<K;++i) {
-        scanf("%d%d",&n,&r);
+        cnt=0;
+        scanf("%d%d", &n, &r);
         n--;
-        int lr = -r,rr = -r;
+        int lr = -r, rr = -r; // 왼쪽 오른쪽은 반대방향으로 회전
         for(int i=n;i>0;--i) { // N극은 0, S극은 1로 나타나있다.
             left_p = gear[i-1][(ind[i-1]+2)%8];
             if(left_p != gear[i][(ind[i]+6)%8])
                 cnt++;// 극이 다르면
             else break;
         }
-        for(int i=n,j=0;i>0&&j<cnt;++j,--i) {
+        for(int i=n,j=0;i>0&&j<cnt;++j,--i) { // 돌리기
             ind[i-1] = (ind[i-1]+8-lr)%8;
-            lr = -lr;
+            lr *= -1;
         }
         cnt=0;
         for(int i=n;i<3;++i) {
             right_p = gear[i+1][(ind[i+1]+6)%8];
             if(right_p != gear[i][(ind[i]+2)%8])
-                // 극이 다르면
-                 cnt++;
+                 cnt++; // 극이 다르면
             else break;
         }
-        for(int i=n,j=0;i<8&&j<cnt;++j,++i) {
+        for(int i=n,j=0;i<3&&j<cnt;++j,++i) {
             ind[i+1] = (ind[i+1]+8-rr)%8;
-            rr = -rr;
+            rr *= -1;
         }
         ind[n] = (ind[n]+8-r)%8; // n 회전
     }
 }
-// 6시 50분
-int main() {
-    for(int i=0;i<4;++i)
-        scanf("%s",gear[i]);
 
-    for(int i=0;i<4;++i)
+int main() {
+    for(int i=0;i<4;++i) {
+        scanf("%s", gear[i]);
         for(int j=0;j<8;++j)
             gear[i][j]-='0';
-    rotate();
-    int k = 1,score=0;
-    for(int i=0;i<4;++i) {
-        if(gear[i][ind[i]] == 1) score += k;
-        k *= 2;
     }
+    rotate();
+    int score = 0;
+    for(int i=0, k=1;i<4;++i,k *= 2) // N극은 0, S극은 1로 나타나있다.
+        if(gear[i][ind[i]]) score += k;
     printf("%d",score);
 }
--- 3190_Snake/main.cpp
+++ 3190_Snake/main.cpp
@@ -1,67 +1,69 @@
 #include <cstdio>
 #include <queue>
-#include <cstring>
 // L: 왼, D: 오
 char a[101][101];
-int N, K, L,dx[] = {1,0,-1,0}, dy[] = {0,1,0,-1};
+int N, K, L, dx[] = {1,0,-1,0}, dy[] = {0,1,0,-1};
 std::pair<int,char> turn[100];
 int last;
+
+void print() {
+    for(int i=1;i<=N;++i) {
+        for(int j=1;j<=N;++j)
+            printf("%2d ",a[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+    fflush(stdout);
+}
+
 bool bound(int r, int c) {return r>0&&r<=N&&c>0&&c<=N;}
 bool hit(int r, int c) {return a[r][c] > 0 && a[r][c] < 5;}
-int sec=0;
+int sec = 0;
 void run() {
-    int h_r=1, h_c=1,n_r = 1,n_c=2,dir=0,t_r=1,t_c=1;
-    int i=0;
+    int h_r=1, h_c=1,n_r = 1,n_c=2,dir=0,t_r=1,t_c=1; // 현재 머리, 새 머리
+    int i = 0;
     a[1][1] = 1;
-    while(!hit(n_r,n_c)&& bound(n_r,n_c)) {
+    while(1) {
 
-        if(i<=last && turn[i].first == sec) {
+        if(i<=last && turn[i].first == sec) { // 돌아야하면
             int d = turn[i].second=='D'?1:-1;
             dir = (dir+4+d)%4;
             i++;
+            a[h_r][h_c] = dir+1; // 회전 저장
         }
-        a[h_r][h_c] = dir+1;
-        n_r = h_r + dy[dir], n_c = h_c + dx[dir];
+        // head 이동
+        n_r = h_r + dy[dir], n_c = h_c + dx[dir];; // new head 이동
+        if(hit(n_r, n_c) || !bound(n_r,n_c)) break;
 
-        if(a[n_r][n_c] != 'a') { // 사과면?
+        if(a[n_r][n_c] != 'a') { // 정상진행
             int tmp_r=t_r, tmp_c = t_c;
             t_r += dy[a[tmp_r][tmp_c]-1], t_c += dx[a[tmp_r][tmp_c]-1];
-            a[tmp_r][tmp_c] = 0;
+            a[tmp_r][tmp_c] = 0; // head == tail이면?
         } // 사과 아니면 tail줄이기
 
-        // head 이동
-        h_r = n_r, h_c = n_c;
-        //n_r = h_r + dy[dir], n_c = h_c + dx[dir]; // new head 이동
-        //if(hit(n_r,n_c)|| !bound(n_r,n_c)) break;
+        a[n_r][n_c] = dir+1; // 이동방향 기억
+        h_r = n_r, h_c = n_c; // 새 위치로 이동
         sec++;
-        for(int i=1;i<=N;++i) {
-            for(int j=1;j<=N;++j)
-                printf("%d ",a[i][j]);
-            printf("\n");
-        }
-        printf("\n");
+        print();
     }
-
 
 }
 int main() {
-    //11시 58분
     int r,c,s;
     char d;
-    scanf("%d %d",&N,&K);
-
+    scanf("%d %d", &N, &K);
     for(int i=0;i<N;++i)
-        memset(a[i],0,N+1);
+        std::fill(a[i], a[i]+N, 0);
     for(int i=0;i<K;++i) {
-        scanf("%d %d",&r,&c);
+        scanf("%d %d", &r, &c);
         a[r][c] = 'a';
     }
-    scanf("%d",&L);
+    scanf("%d", &L);
     for(int i=0;i<L;++i) {
-        scanf("%d %c",&s,&d);
-        turn[i] = {s,d};
+        scanf("%d %c", &s, &d);
+        turn[i] = {s, d};
         last = i;
     }
     run();
-    printf("%d",sec);
+    printf("%d", sec+1);
 }
