b81e76f6d07cdceb57cd819b1a4e39c720bc84ef
YunjaeGong <llegokid@naver.com>
2020-03-03 23:24:04 +0900

    .

---
+++ 3184_Ovce/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+#define f first
+#define s second
+int dx[] = {0,1,0,-1}, dy[] = {1,0,-1,0};
+using namespace std;
+#define pii pair<int,int>
+int r, c;
+int sheep, wolves, res_s, res_w;
+char map[251][251];
+bool visited[251][251];
+inline bool bound(int x, int y) { return x>=0&&x<r&&y>=0&&y<c; }
+void dfs(int x, int y) {
+    visited[x][y] = true;
+    if(map[x][y] == 'v')
+        wolves++;
+    else if(map[x][y] == 'o')
+        sheep++;
+    for(int i=0;i<4;++i) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if(!bound(nx, ny) ||visited[nx][ny] || map[nx][ny] == '#')
+            continue;
+        dfs(nx, ny);
+    }
+
+}
+int main() {
+    cin >> r >> c;
+    queue<pii> Q;
+    for(int i=0;i<r;++i) {
+        cin >> map[i];
+        for(int j=0;j<c;++j)
+            if(map[i][j] == 'v' || map[i][j] == 'o')
+                Q.push({i, j});
+    }
+
+    while(!Q.empty()) {
+        pii cur = Q.front();
+        Q.pop();
+        sheep = 0, wolves = 0;
+        if(visited[cur.f][cur.s])
+            continue;
+        dfs(cur.f, cur.s);
+        if(wolves >= sheep)
+            res_w += wolves;
+        else
+            res_s += sheep;
+    }
+    cout << res_s << " " << res_w;
+}
